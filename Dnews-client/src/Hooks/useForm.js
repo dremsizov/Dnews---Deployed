bdb93767b797ecd953394f27feb1df6560c5716a
Viktor Dremsizov <viktorkz91@gmail.com>
2023-12-09 17:54:21 +0200

    Update useForm.js

--- Dnews-client/src/Hooks/useForm.js
+++ Dnews-client/src/Hooks/useForm.js
@@ -17,13 +17,15 @@ export default function useForm(SubmitHandler, initialValues) {
   const onSubmit = (e) => {
     e.preventDefault();
 
+    
     const trimedData = Object.entries(values).reduce((acc, inputField) => {                                         // Using Object.entries to process each key-value pair in the input object
       const [inputFieldName, inputFieldValue] = inputField;
-
+      
       if (inputFieldValue === '') {                                                               // If the value of the input field is an empty string, skip it in the result
           return acc;
-      }
-
+        }
+        
+    e.currentTarget.reset();
       return { ...acc, [inputFieldName]: typeof inputFieldValue === 'string' ? inputFieldValue.trim() : inputFieldValue };
   }, {})
     
